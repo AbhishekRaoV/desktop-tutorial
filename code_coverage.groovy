9c52b6399e99cce77f212f322166dd6ad7fdc7b6
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2023-12-11 14:53:47 +0530

    Create code_coverage.groovy

---
+++ code_coverage.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+        label 'master'
+    }
+
+    stages {
+        stage('SCM Checkout') {
+            steps {
+                script {
+                    git branch: 'main', url: 'https://github.com/AbhishekRaoV/Augmented_AI'
+                }
+            }
+        }
+        stage("Code Coverage"){
+            steps{
+                script{
+                        dir(Augmented_AI){
+                        sh "cat binarytree.py | sgpt --code \"generate unit test cases\" --no-cache > CodeCoverage.txt  "
+                        }
+                    }
+                }
+            }
+            post{
+                success {
+                    archiveArtifacts artifacts: '**/CodeCoverage.txt'
+                }
+            }
+        }
+    }
+}
