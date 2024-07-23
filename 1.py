8dcfab50afcce7ebbcc53c023aecbcf239596623
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-07-23 17:43:09 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,38 @@
-kubectl apply -f - apiVersion: kubevirt.io/v1alpha1 kind: VirtualMachinePreset metadata:   name: large spec:   selector:     matchLabels:       kubevirt.io/size: large   domain:     resources:       requests:         memory: 1Gi --- apiVersion: kubevirt.io/v1alpha1 kind: OfflineVirtualMachine metadata:   name: ubuntu spec:   running: true   selector:     matchLabels:       guest: ubuntu   template:     metadata:       labels:          guest: ubuntu         kubevirt.io/size: large     spec:       domain:         devices:           disks:             - name: ubuntu               volumeName: ubuntu               disk:                 bus: virtio       volumes:         - name: ubuntu           claimName: ubuntu1710 
+apiVersion: kubevirt.io/v1alpha1
+kind: VirtualMachinePreset
+metadata:
+  name: large
+spec:
+  selector:
+    matchLabels:
+      kubevirt.io/size: large
+  domain:
+    resources:
+      requests:
+        memory: 1Gi
+---
+apiVersion: kubevirt.io/v1alpha1
+kind: OfflineVirtualMachine
+metadata:
+  name: ubuntu
+spec:
+  running: true
+  selector:
+    matchLabels:
+      guest: ubuntu
+  template:
+    metadata:
+      labels:
+        guest: ubuntu
+        kubevirt.io/size: large
+    spec:
+      domain:
+        devices:
+          disks:
+            - name: ubuntu
+              volumeName: ubuntu
+              disk:
+                bus: virtio
+      volumes:
+        - name: ubuntu
+          claimName: ubuntu1710
