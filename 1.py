a7df961b119fe1fb5ccd26c1d4f73ce2cb061682
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-07-24 15:24:02 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,38 +1,64 @@
-apiVersion: kubevirt.io/v1alpha1
-kind: VirtualMachinePreset
-metadata:
-  name: large
-spec:
-  selector:
-    matchLabels:
-      kubevirt.io/size: large
-  domain:
-    resources:
-      requests:
-        memory: 1Gi
 ---
-apiVersion: kubevirt.io/v1alpha1
-kind: OfflineVirtualMachine
+apiVersion: kubevirt.io/v1
+kind: VirtualMachine
 metadata:
-  name: ubuntu
+  name: ubuntu-vm
 spec:
   running: true
-  selector:
-    matchLabels:
-      guest: ubuntu
   template:
     metadata:
       labels:
-        guest: ubuntu
-        kubevirt.io/size: large
+        kubevirt.io/size: small
+        kubevirt.io/domain: ubuntu-vm
     spec:
       domain:
+        cpu:
+          cores: 2
         devices:
           disks:
-            - name: ubuntu
-              volumeName: ubuntu
+            - name: containerdisk
               disk:
                 bus: virtio
+            - name: cloudinitdisk
+              disk:
+                bus: virtio
+          interfaces:
+            - name: default
+              masquerade: {}
+        resources:
+          requests:
+            memory: 8192Mi
+      networks:
+        - name: default
+          pod: {}
       volumes:
-        - name: ubuntu
-          claimName: ubuntu1710
+        - name: containerdisk
+          containerDisk:
+            image: tedezed/ubuntu-container-disk:22.0
+        - name: cloudinitdisk
+          cloudInitNoCloud:
+            userData: |
+              # cloud-config
+              users:
+                - default
+                - name: ubuntu
+                  passwd: "paswrd"
+                  shell: /bin/bash
+                  lock-passwd: false
+                  ssh_pwauth: True
+                  chpasswd: { expire: False }
+                  sudo: ALL=(ALL) NOPASSWD:ALL
+                  groups: users, admin
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: ubuntu-vm-svc
+spec:
+  ports:
+    - port: 22
+      targetPort: 22
+      protocol: TCP
+  selector:
+    kubevirt.io/domain: ubuntu-vm
+  type: NodePort
